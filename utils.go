21c0c32adfed22cce3aca205308655cdb064eb50
prep <maurice@codeninja.nl>
2015-02-14 11:51:45 +0100

    Remove the fallthrough nonsense

--- utils.go
+++ utils.go
@@ -7,31 +7,6 @@ import (
 
 var errEntryNotFound = errors.New("Yaml entry not found")
 
-// fallThrough is a simple channel with a buffer of 1, that can be used as a
-// fallthrough mechanism of select statements.
-type fallThrough struct {
-	C chan struct{}
-}
-
-// newFallThrough creates a new fallThrough object.
-func newFallThrough() *fallThrough {
-	return &fallThrough{C: make(chan struct{}, 1)}
-}
-
-// Clear unsets the fallthrough channel.
-func (ft *fallThrough) Clear() {
-	select {
-	case <-ft.C:
-	default:
-	}
-}
-
-// Set activates the fallthrough channel.
-func (ft *fallThrough) Set() {
-	ft.Clear()
-	ft.C <- struct{}{}
-}
-
 // includesString checks if string _s_ is included in the slice of strings _a_.
 func includesString(a []string, s string) bool {
 	for _, v := range a {
